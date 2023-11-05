a84e980ef764e72e5af021942370d53075d5c83e
Rajchandra Trivedi <trivedirm1@gmail.com>
2023-11-05 18:29:24 +0530

    ArrayCopy file modified

--- Java/Lectures/Day2/src/ArrayCopy.java
+++ Java/Lectures/Day2/src/ArrayCopy.java
@@ -2,7 +2,7 @@
 public class ArrayCopy {
 
 	public static void main(String[] args) {
-		int [] a = {4, 5, 1, 2, 6};
+		int [] a = {4, 5, 3, 4, 6};
 		int [] b = new int[3];
 		
 		// System.arraycopy(sourceArr, sourcepos, destArr, destPos, len)
